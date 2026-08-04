f0a07d54c45d579914d702a9a8d6fd9abbf49b9a
jayvic <jhdream01@gmail.com>
2026-08-05 07:36:23 +0900

    chore(ui): remove duplicate menu drawer instance

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import { RouterView, useRoute } from 'vue-router';
 import DefaultLayout from './components/layouts/DefaultLayout.vue';
 import KbTabBar from './components/common/KbTabBar.vue';
-import KbMenuDrawer from './components/common/KbMenuDrawer.vue';
 
 const route = useRoute();
 </script>
@@ -16,7 +15,6 @@ const route = useRoute();
   <KbTabBar v-if="route.name && !route.meta.hideTabBar" />
   <nav style="display: flex; justify-content: center">
     <router-link to="/guide">UI 가이드 보기</router-link>
-    <KbMenuDrawer />
   </nav>
 </template>
 
