f2abda7b9e4baf01a6317025b80cbfc8bbe5140b
catlover <yewonseoul@gmail.com>
2026-08-13 21:30:30 +0900

    design(asset): add bank logos to account balance page

--- src/router/asset.js
+++ src/router/asset.js
@@ -5,4 +5,10 @@ export default [
     component: () => import('@/pages/asset/AssetDashboard.vue'),
     meta: { title: '자산 관리' },
   },
+  {
+  path: '/asset/balance',
+  name: 'AssetBalance',
+  component: () => import('@/pages/asset/AssetBalance.vue'),
+  meta: { title: '계좌별 잔액' },
+}
 ];
